7200fae75f3bb4039518624d724774aa35b20890
Marcin Warpechowski <warpech@gmail.com>
2015-10-12 19:06:51 +0200

    boolean button

--- ButtonPage.json.cs
+++ ButtonPage.json.cs
@@ -11,5 +11,14 @@ namespace KitchenSink {
         CarrotsReaction = "You have " + action.Value + " imaginary carrots";
       }
     }
+
+    void Handle(Input.EnableCarrotEngine action) {
+      if (action.Value == false) {
+        CarrotEngineReaction = Template.CarrotEngineReaction.DefaultValue;
+      }
+      else {
+        CarrotEngineReaction = "Carrot engine enabled";
+      }
+    }
   }
 }
