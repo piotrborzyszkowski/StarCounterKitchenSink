3c4f1a8471de143d24153650ef6c53ee92fb3242
warpech <warpech@gmail.com>
2015-11-24 11:45:47 +0100

    add Disabled Button

--- ButtonPage.json.cs
+++ ButtonPage.json.cs
@@ -1,24 +1,34 @@
 using Starcounter;
 
 namespace KitchenSink {
-  partial class ButtonPage : Page {
+    partial class ButtonPage : Page {
 
-    void Handle(Input.AddCarrots action) {
-      if (action.Value == 0) {
-        CarrotsReaction = Template.CarrotsReaction.DefaultValue;
-      }
-      else {
-        CarrotsReaction = "You have " + action.Value + " imaginary carrots";
-      }
-    }
+        void Handle(Input.AddCarrots action) {
+            if (action.Value == 0) {
+                CarrotsReaction = Template.CarrotsReaction.DefaultValue;
+            }
+            else {
+                CarrotsReaction = "You have " + action.Value + " imaginary carrots";
+            }
+        }
 
-    void Handle(Input.EnableCarrotEngine action) {
-      if (action.Value == false) {
-        CarrotEngineReaction = Template.CarrotEngineReaction.DefaultValue;
-      }
-      else {
-        CarrotEngineReaction = "Carrot engine enabled";
-      }
+        void Handle(Input.EnableCarrotEngine action) {
+            if (action.Value == false) {
+                CarrotEngineReaction = Template.CarrotEngineReaction.DefaultValue;
+            }
+            else {
+                CarrotEngineReaction = "Carrot engine is on";
+            }
+        }
+
+        void Handle(Input.AddOneCarrot action) {
+            if (action.Value == 0) {
+                OneCarrotReaction = Template.OneCarrotReaction.DefaultValue;
+            }
+            else {
+                OneCarrotReaction = "You have " + action.Value + " imaginary carrots";
+                AddOneCarrotDisabled = true;
+            }
+        }
     }
-  }
 }
