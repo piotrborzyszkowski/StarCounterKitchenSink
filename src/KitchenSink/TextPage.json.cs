c61b9bec9962f220a4e72e0605c225aac054ccaa
warpech <warpech@gmail.com>
2015-12-07 18:16:27 +0100

    add live input listening (https://github.com/Starcounter/Starcounter/issues/3100)

--- src/KitchenSink/TextPage.json.cs
+++ src/KitchenSink/TextPage.json.cs
@@ -1,17 +1,28 @@
 using Starcounter;
 
 namespace KitchenSink {
-  partial class TextPage : Partial {
+    partial class TextPage : Partial {
 
-    public string CalculatedNameReaction {
-      get {
-        if (Name == "") {
-          return "What's your name?";
+        public string CalculatedNameReaction {
+            get {
+                if (Name == "") {
+                    return "What's your name?";
+                }
+                else {
+                    return "Hi, " + Name + "!";
+                }
+            }
         }
-        else {
-          return "Hi, " + Name + "!";
+
+        public string CalculatedNameLiveReaction {
+            get {
+                if (NameLive == "") {
+                    return "What's your name?";
+                }
+                else {
+                    return "Hi, " + NameLive + "!";
+                }
+            }
         }
-      }
     }
-  }
 }
