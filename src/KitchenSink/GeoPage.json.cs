ae8c2cd802599566e6def108075de8abb33f8875
warpech <warpech@gmail.com>
2016-05-17 14:12:54 +0200

    Push marker position in the GeoPage to all sessions (https://github.com/StarcounterSamples/KitchenSink/issues/47)

--- src/KitchenSink/GeoPage.json.cs
+++ src/KitchenSink/GeoPage.json.cs
@@ -1,8 +1,51 @@
 using Starcounter;
 
 namespace KitchenSink {
-  partial class GeoPage : Json
-    {
-    
-  }
+    partial class GeoPage : Json {
+
+    }
+
+    //TODO change me to a database class (then the below setters and getters can be removed)
+    public static class SharedPosition {
+        public static double Latitude = 59.3319913;
+        public static double Longitude = 18.0765409;
+    }
+
+    [GeoPage_json.Position]
+    partial class GeoPagePosition : Json {
+        public double Latitude {
+            set {
+                SharedPosition.Latitude = value;
+                PushChanges();
+            }
+            get {
+                return SharedPosition.Latitude;
+            }
+        }
+
+        public double Longitude {
+            set {
+                SharedPosition.Longitude = value;
+                PushChanges();
+            }
+            get {
+                return SharedPosition.Longitude;
+            }
+        }
+
+        protected void PushChanges() {
+            Session.ForAll((Session s, string sessionId) => {
+                MasterPage master = s.Data as MasterPage;
+                if (master != null && master.CurrentPage is NavPage) {
+                    NavPage navpage = (NavPage)master.CurrentPage;
+                    if (navpage.CurrentPage is GeoPage) {
+                        GeoPage page = (GeoPage)navpage.CurrentPage;
+                        if (page != null) {
+                            s.CalculatePatchAndPushOnWebSocket();
+                        }
+                    }
+                }
+            });
+        }
+    }
 }
