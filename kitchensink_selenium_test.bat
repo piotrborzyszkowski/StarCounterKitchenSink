4d047dab52269c56f44cb1a1d75cc070f6d47b1e
alemoi <alexey.moiseenko@starcounter.com>
2016-05-26 15:15:41 +0200

    Added run on a specific database.

--- kitchensink_selenium_test.bat
+++ kitchensink_selenium_test.bat
@@ -1,6 +1,6 @@
 IF "%Configuration%"=="" set Configuration=Debug
 
-star --resourcedir="%~dp0src\KitchenSink\wwwroot" "%~dp0bin/%Configuration%/KitchenSink.exe"
+star -d=kitchensink --resourcedir="%~dp0src\KitchenSink\wwwroot" "%~dp0bin/%Configuration%/KitchenSink.exe"
 IF ERRORLEVEL 1 EXIT /b 1
 
 packages\NUnit.ConsoleRunner.3.2.0\tools\nunit3-console.exe test\KitchenSink.Tests\KitchenSink.Tests.csproj /config:%Configuration%
