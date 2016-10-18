7e28c39fc20358c5c6f4de365fa84f313f69a5fd
Dimitris Thanasis <Dimitris.th@hotmail.com>
2016-10-18 14:54:30 +0200

    Disabled the button while a task is running
    Added a boolean check, which dictates if a scheduled task is running or
    not, and if its running, it should disable the button from being pressed
    again and thus creating a new task
    
    Enhancement for StarcounterSamples/KitchenSink#67

--- src/KitchenSink/ProgressBarPage.json.cs
+++ src/KitchenSink/ProgressBarPage.json.cs
@@ -11,43 +11,43 @@ namespace KitchenSink
 
         void Handle(Input.StartProgress action) // Button Input
         {
+            if (this.TaskIsRunnning)
+            {
+                return;
+            }
+            this.TaskIsRunnning = true;
             string sessionId = Session.SessionId;
 
-            if (this.Progress == 0 || this.Progress == 100)
-            {
-                this.Progress = 0;
-                StartSimpleProgressBar(30, sessionId);
-            }
-
             this.Progress = 0;
+            StartSimpleProgressBar(30, sessionId);
+
         }
 
         void StartSimpleProgressBar(int delay, string sessionId)
         {
             this.FileDownloadText = "Downloading File";
-
+            var tempProgress = this.Progress;
             Scheduling.ScheduleTask(() =>
             {
-                while (this.Progress < 100)
+                while (tempProgress < 100)
                 {
                     System.Threading.Thread.CurrentThread.Join(delay); // sleep function - handles the delay between the incrementation of this.Progress
-                    SimpleProgressUpdate(sessionId);
+                    tempProgress++;
+                    SimpleProgressUpdate(sessionId, tempProgress);
                 }
             }, false); // Wait for completion - If false: it will continue to run the script even though the scheduled task is running in the background
         }
 
-        void SimpleProgressUpdate(string sessionId)
+        void SimpleProgressUpdate(string sessionId, long tempProgress)
         {
             Session.ScheduleTask(sessionId, (s, id) =>
             {
-                if (this.Progress < 100)
-                {
-                    this.Progress++;
-                }
-                else
+                this.Progress = tempProgress;
+                if (this.Progress >= 100)
                 {
                     this.FileDownloadText = "Download Complete";
                     this.DownloadButtonText = "Download another (imaginary) file!";
+                    this.TaskIsRunnning = false;
                 }
                 s.CalculatePatchAndPushOnWebSocket();
             });
