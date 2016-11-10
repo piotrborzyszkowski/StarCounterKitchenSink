95b3bd47920781dc29123434f824a2a7479d91e1
Erlend <elandroe@icloud.com>
2016-11-10 13:24:56 +0100

    new tests and reorganization

--- src/KitchenSink/PaginationPage.json.cs
+++ src/KitchenSink/PaginationPage.json.cs
@@ -38,8 +38,56 @@ namespace KitchenSink
             InitPage();
         }
 
+        // Changes the number of entries per page depending on input and sets the page to 1
+        void Handle(Input.EntriesPerPage action)
+        {
+            this.CurrentOffset = 0;
+            this.EntriesPerPage = action.Value;
+            SetTotalPages();
+            GetNewPage();
+        }
+
+        // Sets the current page when the user clicks on one of the numbered buttons
+        void Handle(Input.ChangePage action)
+        {
+            this.CurrentOffset = this.EntriesPerPage * (action.Value - 1);
+            GetNewPage();
+        }
+
+        // Goes to the next page on click
+        void Handle(Input.NextPage action)
+        {
+            if (this.CurrentOffset + this.EntriesPerPage < this.TotalEntries)
+            {
+                this.CurrentOffset = this.CurrentOffset + this.EntriesPerPage;
+            }
+            GetNewPage();
+        }
+
+        // Goes to the previous page on click
+        void Handle(Input.PreviousPage action)
+        {
+            this.CurrentOffset = this.CurrentOffset - this.EntriesPerPage >= 0 ? this.CurrentOffset - this.EntriesPerPage : 0;
+            GetNewPage();
+        }
+
+        // Goes to the last page on click
+        void Handle(Input.LastPage action)
+        {
+            var remainder = this.TotalEntries % this.EntriesPerPage;
+            this.CurrentOffset = remainder != 0 ? this.TotalEntries - (remainder) : this.TotalEntries - this.EntriesPerPage;
+            GetNewPage();
+        }
+
+        // Goes to the first page on click
+        void Handle(Input.FirstPage action)
+        {
+            this.CurrentOffset = 0;
+            GetNewPage();
+        }
+
         // Initializes the page by calling all the neccesary methods to set the page to 1
-        public void InitPage()
+        private void InitPage()
         {
             GetNewPage();
             SetTotalPages();
@@ -49,7 +97,7 @@ namespace KitchenSink
 
         // Creates the choices for page entries.
         // Modify the array entryChoices to modify how many entries per page the user can choose between.
-        public void SetPageEntries()
+        private void SetPageEntries()
         {
             int[] entryChoices = new int[] { 5, 15, 30 };
             foreach (int entry in entryChoices)
@@ -60,7 +108,7 @@ namespace KitchenSink
         }
 
         // Sets the JSON value for total pages    
-        public void SetTotalPages()
+        private void SetTotalPages()
         {
             var totalPagesRounded = this.TotalEntries / this.EntriesPerPage;
             this.TotalPages = this.TotalEntries % this.EntriesPerPage == 0 ? totalPagesRounded : totalPagesRounded + 1;
@@ -69,7 +117,7 @@ namespace KitchenSink
         // Establishes the navigation buttons.
         // Displays all the page buttons if there are less then 10,
         // Otherwise it shows the current page, the two before and after.
-        public void CreateNavButtons(long entriesPerPage)
+        private void CreateNavButtons(long entriesPerPage)
         {
             this.Pages.Clear();
             this.CurrentPage = this.CurrentOffset / entriesPerPage + 1;
@@ -118,58 +166,10 @@ namespace KitchenSink
         }
 
         // Switch to the desired page depeding on the current offset.
-        public void GetNewPage()
+        private void GetNewPage()
         {
             this.Library.Data = Db.SQL<Book>("SELECT b FROM KitchenSink.Book b FETCH ? OFFSET ?", this.EntriesPerPage, this.CurrentOffset);
             CreateNavButtons(this.EntriesPerPage);
-        }
-
-        // Changes the number of entries per page depending on input and sets the page to 1
-        void Handle(Input.EntriesPerPage action)
-        {
-            this.CurrentOffset = 0;
-            this.EntriesPerPage = action.Value;
-            SetTotalPages();
-            GetNewPage();
-        }
-
-        // Sets the current page when the user clicks on one of the numbered buttons
-        void Handle(Input.ChangePage action)
-        {
-            this.CurrentOffset = this.EntriesPerPage * (action.Value - 1);
-            GetNewPage();
-        }
-
-        // Goes to the next page on click
-        void Handle(Input.NextPage action)
-        {
-            if (this.CurrentOffset + this.EntriesPerPage < this.TotalEntries)
-            {
-                this.CurrentOffset = this.CurrentOffset + this.EntriesPerPage;
-            }
-            GetNewPage();
-        }
-
-        // Goes to the previous page on click
-        void Handle(Input.PreviousPage action)
-        {
-            this.CurrentOffset = this.CurrentOffset - this.EntriesPerPage >= 0 ? this.CurrentOffset - this.EntriesPerPage : 0;
-            GetNewPage();
-        }
-
-        // Goes to the last page on click
-        void Handle(Input.LastPage action)
-        {
-            var remainder = this.TotalEntries % this.EntriesPerPage;
-            this.CurrentOffset = remainder != 0 ? this.TotalEntries - (remainder) : this.TotalEntries - this.EntriesPerPage;
-            GetNewPage();
-        }
-
-        // Goes to the first page on click
-        void Handle(Input.FirstPage action)
-        {
-            this.CurrentOffset = 0;
-            GetNewPage();
         }
     }
 }
--- test/KitchenSink.Tests/Tests/PaginationPageTest.cs
+++ test/KitchenSink.Tests/Tests/PaginationPageTest.cs
@@ -17,112 +17,109 @@ namespace KitchenSink.Test
         {
         }
 
+        [SetUp]
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/pagination");
+            this.WaitUntil(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("puppet-client")));
+        }
+
         [Test]
         public void PaginationPage_PageLoads()
         {
             driver.Navigate().GoToUrl(baseURL);
 
             driver.FindElement(ByHelper.AnyLinkWithText("Pagination")).ClickUsingMouse(driver);
-
-            var element = driver.FindElements(By.ClassName("kitchensink-pagination-entry"));
-            Assert.IsNotEmpty(element);
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.ClassName("kitchensink-pagination-entry")));
         }
 
         [Test]
-        public void PaginationPage_DropdownTest()
+        public void PaginationPage_Dropdown_HasCorrectOptions()
         {
-            driver.Navigate().GoToUrl(baseURL + "/pagination");
-            var dropDown = driver.FindElement(By.TagName("select"));
-            var dropDownOptions = dropDown.FindElements(By.TagName("option"));
-            Assert.AreEqual(dropDownOptions[0].Text, "5");
-            Assert.AreEqual(dropDownOptions[1].Text, "15");
+            var dropDownOptions = driver.FindElements(By.XPath("//select/option"));
+            string[] entriesPerPage = new string[] { "5", "15", "30" };
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(dropDownOptions[i].Text, entriesPerPage[i]);
+            }
         }
 
-        public void ClickButton(string buttonID, string initialElementText)
+        [Test]
+        public void PaginationPage_DropDown_RightNumberOfElementsOnClick()
+        {
+            var dropDownOptions = driver.FindElements(By.XPath("//select/option"));
+            int[] entriesPerPage = new int[] { 5, 15, 30 };
+            for (int i = 0; i < dropDownOptions.Count; i++)
+            {
+                var dropDown = driver.FindElement(By.TagName("select"));
+                var dropDownSelect = new SelectElement(dropDown);
+                dropDownSelect.SelectByIndex(i);
+
+                wait.Until((x) => x.FindElements(By.ClassName("kitchensink-pagination-entry")).Count.Equals(entriesPerPage[i]));
+            }
+        }
+
+        [Test]
+        public void PaginationPage_LastButton_GoesToLastPage()
+        {
+            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
+            ClickButton("kitchensink-pagination-last", initialElementText);
+
+            Assert.AreEqual(driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text, "Arbitrary Book 96 - Arbitrary Author");
+        }
+
+
+        // This test is dependent on the last button
+        [Test]
+        public void PaginationPage_FirstButton_GoesToFirstPage()
+        {
+            ClickButton("kitchensink-pagination-last", driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
+            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
+            ClickButton("kitchensink-pagination-first", initialElementText);
+            Assert.AreEqual(driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text, "Arbitrary Book 1 - Arbitrary Author");
+        }
+
+        [Test]
+        public void PaginationPage_NextButton_GoesToNextPage()
+        {
+            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
+            ClickButton("kitchensink-pagination-next", initialElementText);
+            Assert.AreEqual(driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text, "Arbitrary Book 6 - Arbitrary Author");
+        }
+
+        // This test is dependent on the next button
+        [Test]
+        public void PaginationPage_PreviousButton_GoesToPreviousPage()
+        {
+            ClickButton("kitchensink-pagination-next", driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
+            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
+            ClickButton("kitchensink-pagination-previous", initialElementText);
+            Assert.AreEqual(driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text, "Arbitrary Book 1 - Arbitrary Author");
+        }
+
+        [Test]
+        public void PaginationPage_NavButtons_GoesToRightPage()
+        {
+            var buttons = driver.FindElements(By.ClassName("kitchensink-pagination-change"));
+            for (int i = 0; i < 3; i++)
+            {
+                buttons[i].ClickUsingMouse(driver);
+                wait.Until((x) => {
+                    var expectedFirstEntryText = "Arbitrary Book " + (Int32.Parse(buttons[i].Text) * 5 - 4).ToString() + " - Arbitrary Author";
+                    var firstEntryText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
+                    return expectedFirstEntryText == firstEntryText;
+               });
+            }
+
+        }
+        
+        private void ClickButton(string buttonID, string initialElementText)
         {
             driver.FindElement(By.Id(buttonID)).ClickUsingMouse(driver);
             wait.Until((x) => {
                 var currentText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
                 return !initialElementText.Equals(currentText);
             });
-        }
-
-        [Test]
-        public void PaginationPage_LastButtonTest()
-        {
-            driver.Navigate().GoToUrl(baseURL + "/pagination");
-
-            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
-            ClickButton("kitchensink-pagination-last", initialElementText);
-            Assert.AreNotEqual(initialElementText, driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-        }
-
-
-        // This test is dependent on the last button
-        [Test]
-        public void PaginationPage_FirstButtonTest()
-        {
-            driver.Navigate().GoToUrl(baseURL + "/pagination");
-
-            ClickButton("kitchensink-pagination-last", driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
-            ClickButton("kitchensink-pagination-first", initialElementText);
-            Assert.AreNotEqual(initialElementText, driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-        }
-
-        [Test]
-        public void PaginationPage_NextButtonTest()
-        {
-            driver.Navigate().GoToUrl(baseURL + "/pagination");
-
-            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
-            ClickButton("kitchensink-pagination-next", initialElementText);
-            Assert.AreNotEqual(initialElementText, driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-        }
-
-        // This test is dependent on the next button
-        [Test]
-        public void PaginationPage_PreviousButtonTest()
-        {
-            driver.Navigate().GoToUrl(baseURL + "/pagination");
-
-            ClickButton("kitchensink-pagination-next", driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-            var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
-            ClickButton("kitchensink-pagination-previous", initialElementText);
-            Assert.AreNotEqual(initialElementText, driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-        }
-
-        [Test]
-        public void PaginationPage_NavButtonTests()
-        {
-            driver.Navigate().GoToUrl(baseURL + "/pagination");
-
-            var buttons = driver.FindElements(By.ClassName("kitchensink-pagination-change"));
-
-            foreach (var button in buttons)
-            {
-                System.Diagnostics.Debug.Write("HERE");
-                var initialElementText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
-                button.ClickUsingMouse(driver);
-                wait.Until((x) => {
-                    var currentText = driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text;
-                    if (button.Text == "1")
-                    {
-                        return initialElementText.Equals(currentText);
-                    }
-                    return !initialElementText.Equals(currentText);
-                });
-                // If you get through the wait.Until, then it must be true 
-                // could probabaly replace with Assert.True(true) without any issues
-                if (button.Text == "1")
-                {
-                    Assert.AreEqual(initialElementText, driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-                }
-                else
-                {
-                    Assert.AreNotEqual(initialElementText, driver.FindElement(By.ClassName("kitchensink-pagination-entry")).Text);
-                }
-            }
         }
     }
 }
