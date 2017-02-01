4dd2e14680990f68f978701615077ad1603e70b4
Krystian Matti <krystian.matti@starcounter.com>
2017-02-01 10:23:08 +0100

    fix for project renaming

--- test/KitchenSink.Tests/Properties/AssemblyInfo.cs
+++ test/KitchenSink.Tests/Properties/AssemblyInfo.cs
@@ -5,25 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("KitchenSink.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KitchenSink.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("24c88d95-851a-4786-bff2-7b5ca2b84ee9")]
+[assembly: Guid("b10a99cc-66cf-49ec-b9df-51f9966f1922")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
