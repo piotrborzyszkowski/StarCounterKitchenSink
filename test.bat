85a7d01f2e5e8bf95478d6e533d2e8a48fcdd1d4
Krystian Matti <krystian.matti@starcounter.com>
2017-03-10 17:10:19 +0100

    - Change comment - Delete unnecessary loop

--- test.bat
+++ test.bat
@@ -4,13 +4,11 @@ set interactive=1
 echo %cmdcmdline% | find /i "%~0" >nul
 if not errorlevel 1 set interactive=0
 
-rem now I can use %interactive% anywhere
+rem If we are in interactive mode (batch file not started from the command line), pause at the exit
 
 IF NOT EXIST "%~dp0packages\NUnit.ConsoleRunner.3.6.1\" (ECHO Error: Cannot find NUnit Console Runner. Build the project to restore the packages && PAUSE && EXIT /B 1)
-%~dp0packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe %~dp0test\KitchenSink.Tests\bin\Debug\KitchenSink.Tests.dll --noheader --params Browsers=Chrome,Firefox
+%~dp0packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe %~dp0test\KitchenSink.Tests\bin\Debug\KitchenSink.Tests.dll --noheader --params Browsers=Chrome
 
-if _%1_==__ (
-    if _%interactive%_==_0_ pause
-)
+if %interactive%==0 pause
 
 IF ERRORLEVEL 1 EXIT /B 1
