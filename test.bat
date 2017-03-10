3c07414123b0e31a3b54ffcfd8d12776e8ee5993
Krystian Matti <krystian.matti@starcounter.com>
2017-03-10 16:17:02 +0100

    Fixes and updates - fix for test.bat, don't use "pause" command when running script from console - updated nunit console runner - updated webdriver lib

--- test.bat
+++ test.bat
@@ -1,6 +1,16 @@
 @echo off
 
-IF NOT EXIST "%~dp0packages\NUnit.ConsoleRunner.3.6.0\" (ECHO Error: Cannot find NUnit Console Runner. Build the project to restore the packages && PAUSE && EXIT /B 1)
-%~dp0packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe %~dp0test\KitchenSink.Tests\bin\Debug\KitchenSink.Tests.dll --noheader --params Browsers=Chrome,Firefox
-pause
+set interactive=1
+echo %cmdcmdline% | find /i "%~0" >nul
+if not errorlevel 1 set interactive=0
+
+rem now I can use %interactive% anywhere
+
+IF NOT EXIST "%~dp0packages\NUnit.ConsoleRunner.3.6.1\" (ECHO Error: Cannot find NUnit Console Runner. Build the project to restore the packages && PAUSE && EXIT /B 1)
+%~dp0packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe %~dp0test\KitchenSink.Tests\bin\Debug\KitchenSink.Tests.dll --noheader --params Browsers=Chrome,Firefox
+
+if _%1_==__ (
+    if _%interactive%_==_0_ pause
+)
+
 IF ERRORLEVEL 1 EXIT /B 1
