f757cfa3404785997e0e828315c0ff1e8af94c9b
Krystian Matti <krystian.matti@starcounter.com>
2017-03-17 10:30:35 +0100

    Uniform batch scripts as presented in guid - eddited build.bat, run.bat, test.bat, package.bat - moved package.bat from tools folder

--- test.bat
+++ test.bat
@@ -1,11 +1,21 @@
-@echo off
+@ECHO OFF
 
-set interactive=1
-echo %cmdcmdline% | find /i "%~0" >nul
-if not errorlevel 1 set interactive=0
+staradmin stop db default
 
-rem If we are in interactive mode (batch file not started from the command line), pause at the exit
+IF "%CONFIGURATION%"=="" SET CONFIGURATION=Debug
 
+SET INTERACTIVE=1
+ECHO %CMDCMDLINE% | find /i "%~0" >nul
+IF NOT ERRORLEVEL 1 SET INTERACTIVE=0
+
+:: Stop all the running apps
+staradmin stop db default
+
+:: Start the tested app
+star --resourcedir="%~dp0src\KitchenSink\wwwroot" "%~dp0bin/%CONFIGURATION%/KitchenSink.exe"
+IF ERRORLEVEL 1 EXIT /B 1
+
+:: Start the test
 IF NOT EXIST "%~dp0packages\NUnit.ConsoleRunner.3.6.1\" (ECHO Error: Cannot find NUnit Console Runner. Build the project to restore the packages && PAUSE && EXIT /B 1)
 %~dp0packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe %~dp0test\KitchenSink.Tests\bin\Debug\KitchenSink.Tests.dll --noheader --params Browsers=Chrome
 
